4d46b0ff2f84337c78f1f8e2a337096b3ec3c102
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-12 14:49:56 +0000

    Added item description and quantity

--- src/item.py
+++ src/item.py
@@ -1,5 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Item:
-  def __init__(self, name):
+  def __init__(self, name, description, qty = 1):
     self.name = name
+    self.description = description
+    self.qty = qty
