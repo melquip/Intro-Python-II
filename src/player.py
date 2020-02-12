00a76eb19b39f7521354f0bda84a88d4f59932ce
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-12 14:50:14 +0000

    Added player and room item management system

--- src/player.py
+++ src/player.py
@@ -1,9 +1,42 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
 class Player:
   def __init__(self, room, inventory = []):
     self.room = room
     self.inventory = inventory
 
-  def goToRoom(self, room, inventory):
+  def goToRoom(self, room):
     self.room = room
+
+  def getItem(self, loot, qty = 1):
+    # remove item from room
+    availableQty = self.room.dropItem(loot, qty)
+    # add item to player inventory
+    if availableQty > 0:
+      if loot.name not in [item.name for item in self.inventory]:
+        self.inventory.append(Item(loot.name, loot.description, availableQty))
+      else:
+        for item in self.inventory:
+          if item.name == loot.name:
+            item.qty += availableQty
+    else:
+      print('No such item available in this room!')
+
+  def dropItem(self, loot, qty = 1):
+    # remove item from player inventory
+    availableQty = 0
+    for i, item in enumerate(self.inventory):
+      if item.name == loot.name:
+        if item.qty >= qty:
+          availableQty = qty
+        else:
+          availableQty = qty + (item.qty - qty)
+        item.qty -= availableQty
+        if item.qty <= 0:
+          self.inventory.pop(i)
+    # add item to player room
+    if availableQty > 0:
+      self.room.getItem(loot, availableQty)
+    else:
+      print('Can\'t drop an item that\'s not in your possession!')
--- src/room.py
+++ src/room.py
@@ -1,8 +1,30 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
 class Room:
   def __init__(self, name, description, key, items = []):
     self.key = key
     self.name = name
     self.description = description
     self.items = items
+
+  def getItem(self, loot, qty = 1):
+    if loot.name not in [item.name for item in self.items]:
+      self.items.append(Item(loot.name, loot.description, qty))
+    else:
+      for item in self.items:
+        if item.name == loot.name:
+          item.qty += qty
+
+  def dropItem(self, loot, qty = 1):
+    availableQty = 0
+    for i, item in enumerate(self.items):
+      if item.name == loot.name:
+        if item.qty >= qty:
+          availableQty = qty
+        else:
+          availableQty = qty + (item.qty - qty)
+        item.qty -= availableQty
+        if item.qty <= 0:
+          self.items.pop(i)
+    return availableQty
