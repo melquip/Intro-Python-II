ae49e46520afc93d727dc5e81faec50d32b5a498
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-12 14:49:17 +0000

    Added items list and multiple commands

--- src/adv.py
+++ src/adv.py
@@ -1,33 +1,69 @@
+from item import Item
 from room import Room
 from player import Player
 
 # Declare all the rooms
+items = [
+  Item('Napkin', 'A damaged piece of cloth napkin.', 1),
+  Item('Sword', 'A good-looking weapon.', 1),
+  Item('Rock', 'Rubble from ancient structures.', 3),
+  Item('Coin', 'A golden coin from an ancient civilization.', 2),
+]
 
-room = {
-  'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons", 'outside'),
-  'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", 'foyer'),
-  'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+def createItem(i):
+  item = items[i]
+  return Item(item.name, item.description, item.qty)
+
+rooms = {
+  'outside': Room(
+    "Outside Cave Entrance", 
+    "North of you, the cave mount beckons", 
+    'outside',
+    [createItem(2)]
+  ),
+  'foyer': Room(
+    "Foyer",
+    """Dim light filters in from the south. Dusty
+passages run north and east.""",
+    'foyer',
+    [createItem(0), createItem(2)]
+  ),
+  'overlook': Room(
+    "Grand Overlook",
+    """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", 'overlook'),
-  'narrow': Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", 'narrow'),
-  'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+the distance, but there is no way across the chasm.""",
+  'overlook',
+  [createItem(2)]
+  ),
+  'narrow': Room(
+    "Narrow Passage",
+    """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""",
+    'narrow',
+    [createItem(1), createItem(2)]
+  ),
+  'treasure': Room(
+    "Treasure Chamber",
+    """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", 'treasure'),
+earlier adventurers. The only exit is to the south.""",
+    'treasure',
+    [createItem(3)]
+  ),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+rooms['outside'].n_to = rooms['foyer']
+rooms['foyer'].s_to = rooms['outside']
+rooms['foyer'].n_to = rooms['overlook']
+rooms['foyer'].e_to = rooms['narrow']
+rooms['overlook'].s_to = rooms['foyer']
+rooms['narrow'].w_to = rooms['foyer']
+rooms['narrow'].n_to = rooms['treasure']
+rooms['treasure'].s_to = rooms['narrow']
 
 #
 # Main
@@ -45,38 +81,84 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-
+allDirections = ['N', 'E', 'S', 'W']
+availableDirections = []
 initialRoom = 'outside'
-player1 = Player(room[initialRoom])
+player1 = Player(rooms[initialRoom], [])
 
 quitGame = False
-while not quitGame:
+
+def goDirection(direction):
+  global player1, allDirections, quitGame
+  if direction == 'N' and direction in availableDirections:
+    player1.goToRoom(player1.room.n_to)
+  elif direction == 'E' and direction in availableDirections:
+    player1.goToRoom(player1.room.e_to)
+  elif direction == 'S' and direction in availableDirections:
+    player1.goToRoom(player1.room.s_to)
+  elif direction == 'W' and direction in availableDirections:
+    player1.goToRoom(player1.room.w_to)
+  elif direction == 'Q':
+    print("Giving up already? Weak adventurers shouldn't even have started!")
+    quitGame = True
+  elif direction in allDirections and direction not in availableDirections:
+    print("Congrats! You just smashed your face on the wall!")
+  elif direction not in allDirections:
+    print("You seem confused. Try N, E, S or W!")
+
+def tutorial():
+  print('Available commands:')
+  print('go <dir>, where <dir> is N, E, S, W, to change rooms')
+  print('get <qty> <item-name>, to pick up <qty>x of <item-name>')
+  print('drop <qty> <item-name>, to drop <qty>x of <item-name>')
   print('\n')
+
+tutorial()
+while not quitGame:
+  # player current location
   print(f"You walked into the Room {player1.room.name}")
   print(f"{player1.room.description}\n")
 
-  allDirections = ['N', 'E', 'S', 'W']
+  # directions you can travel
   availableDirections = [key.split("_")[0].upper() for key, val in vars(player1.room).items() if key not in ['name', 'description', 'key']]
+  for direction in allDirections:
+    if direction in availableDirections:
+      print(f'[{direction}] Looks like a promising path...')
+    else:
+      print(f'[{direction}] Doesn\'t inpire confidence...')
   
-  for direction in availableDirections:
-    print(f'[{direction}]')
+  # room items
+  print('\nHere\'s what you can see:')
+  for item in player1.room.items:
+    print(f'{item.qty}x [{item.name}] - {item.description}')
+
+  # player inventory 
+  print('\nHere\'s what you have:')
+  for item in player1.inventory:
+    print(f'{item.qty}x [{item.name}] - {item.description}')
+
+  # player move
+  print('\n')
+  allInput = input("What do you do?\n")
+  userInput = allInput.split(' ')[1:]
+  userCommand = allInput.split(' ')[:1][0].upper()
+  print('\n')
+
+  # execute correct player command
+  if userCommand == 'GO':
+    goDirection(userInput[0].upper())
+  elif userCommand == 'GET':
+    inputItem = ''.join(userInput[1:]).lower().capitalize()
+    inputItem = [item for item in items if item.name == inputItem][0]
+    inputQty = int(userInput[:1][0])
+    player1.getItem(inputItem, inputQty)
+  elif userCommand == 'DROP':
+    inputItem = ''.join(userInput[1:]).lower().capitalize()
+    inputItem = [item for item in items if item.name == inputItem][0]
+    inputQty = int(userInput[:1][0])
+    player1.dropItem(inputItem, inputQty)
+  else:
+    print('That command is invalid.')
+    tutorial()
 
   print('\n')
-  decision = input("What do you do?\n").upper()
-  print('\n')
-
-  if decision == 'N' and decision in availableDirections:
-    player1.goToRoom(player1.room.n_to)
-  elif decision == 'E' and decision in availableDirections:
-    player1.goToRoom(player1.room.e_to)
-  elif decision == 'S' and decision in availableDirections:
-    player1.goToRoom(player1.room.s_to)
-  elif decision == 'W' and decision in availableDirections:
-    player1.goToRoom(player1.room.w_to)
-  elif decision == 'Q':
-    print("Giving up already? Weak adventurers shouldn't even have started!")
-    quitGame = True
-  elif decision in allDirections and decision not in availableDirections:
-    print("Congrats! You just smashed your face on the wall!")
-  elif decision not in allDirections:
-    print("You seem confused. Try N, E, S or W!")
